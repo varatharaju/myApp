eaf7b5e8c8ac69eab993e59705658be5efa58108
varatharaju <shanmugam.varatharaj@gmail.com>
2017-03-03 16:04:04 +0530

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :check_session, except: ["login","verify_login","logout"]
-  skip_before_filter :verify_authenticity_token
 
   private
   def check_session
